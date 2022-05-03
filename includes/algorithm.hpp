dd8333035810df92bc1b7b7c0e8261876cdc42d2
anclarma <anclarma@student.42.fr>
2022-05-03 03:07:16 +0200

    documentation algorithm

--- includes/algorithm.hpp
+++ includes/algorithm.hpp
@@ -6,7 +6,7 @@
 /*   By: anclarma <anclarma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 03:06:35 by anclarma          #+#    #+#             */
-/*   Updated: 2022/04/05 03:31:41 by anclarma         ###   ########.fr       */
+/*   Updated: 2022/05/03 02:53:37 by anclarma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,59 +16,58 @@
 namespace	ft
 {
 	//equal
+	//Returns true if the range [first1, last1) is equal to the range [first2, first2 + (last1 - first1)), and false otherwise
+	//Complexity:
+	// At most last1 - first1 applications of the predicate
 	template <class InputIterator1, class InputIterator2>
 		bool	equal(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2)
 		{
-			while (first1 != last1)
-			{
+			for (; first1 != last1; ++first1, ++first2)
 				if (!(*first1 == *first2))
 					return (false);
-				++first1;
-				++first2;
-			}
 			return (true);
 		}
 	template <class InputIterator1, class InputIterator2, class BinaryPredicate>
 		bool	equal(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, BinaryPredicate pred)
 		{
-			while (first1 != last1)
-			{
+			for (; first1 != last1; ++first1, ++first2)
 				if (!pred(*first1, *first2))
 					return (false);
-				++first1;
-				++first2;
-			}
 			return (true);
 		}
 
 	//lexicographical_compare
+	//Elements are compared using operator<
+	//Complexity:
+	// At most 2·min(N1, N2) applications of the comparison operation,
+	// where N1 = std::distance(first1, last1) and N2 = std::distance(first2, last2)
 	template <class InputIterator1, class InputIterator2>
-		bool lexicographical_compare (InputIterator1 first1, InputIterator1 last1,
+		bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1,
 				InputIterator2 first2, InputIterator2 last2)
 		{
-			while (first1 != last1)
+			for (; first1 != last1; ++first1, ++first2)
 			{
 				if (first2 == last2 || *first2 < *first1)
 					return (false);
 				else if (*first1 < *first2)
 					return (true);
-				++first1;
-				++first2;
 			}
 			return (first2 != last2);
 		}
+	//Elements are compared using the given binary comparison function comp
+	//Complexity:
+	// At most 2·min(N1, N2) applications of the comparison operation,
+	// where N1 = std::distance(first1, last1) and N2 = std::distance(first2, last2)
 	template <class InputIterator1, class InputIterator2, class Compare>
-		bool lexicographical_compare (InputIterator1 first1, InputIterator1 last1,
+		bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1,
 				InputIterator2 first2, InputIterator2 last2, Compare comp)
 		{
-			while (first1 != last1)
+			for (; first1 != last1; ++first1, ++first2)
 			{
 				if (first2 == last2 || comp(*first2, *first1))
 					return (false);
 				else if (comp(*first1, *first2))
 					return (true);
-				++first1;
-				++first2;
 			}
 			return (first2 != last2);
 		}
--- includes/utility.hpp
+++ includes/utility.hpp
@@ -6,7 +6,7 @@
 /*   By: anclarma <anclarma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/19 13:00:58 by anclarma          #+#    #+#             */
-/*   Updated: 2022/04/19 17:27:56 by anclarma         ###   ########.fr       */
+/*   Updated: 2022/05/03 03:06:49 by anclarma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,29 +15,30 @@
 
 namespace	ft
 {
-	template <class T1, class T2> struct pair
-	{
-		public:
-			// Member types
-			typedef T1	first_type;
-			typedef T2	second_type;
-			// Member variables
-			first_type	first;
-			second_type	second;
-			// (constructor)
-			pair() : first(), second()
+	template <class T1, class T2>
+		struct pair
 		{
-		}
-			template<class U, class V>
-				pair(const pair<U,V>& pr) : first(pr.first), second(pr.second)
+			public:
+				// Member types
+				typedef T1	first_type;
+				typedef T2	second_type;
+				// Member variables
+				first_type	first;
+				second_type	second;
+				// (constructor)
+				pair() : first(), second()
 			{
 			}
-			pair (const first_type& a, const second_type& b) : first(a), second(b)
-		{
-		}
-			// pair::operator=
-			// implicitly declared
-	};
+				template<class U, class V>
+					pair(const pair<U,V>& pr) : first(pr.first), second(pr.second)
+				{
+				}
+				pair (const first_type& a, const second_type& b) : first(a), second(b)
+			{
+			}
+				// pair::operator=
+				// implicitly declared
+		};
 	//relational operators (pair)
 	template <class T1, class T2>
 		bool operator== (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
